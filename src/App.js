4999243b96450a25f00501cb3d468f9e65b9b7dd
后厂村小王 <48429995+EricWong1994@users.noreply.github.com>
2022-05-19 08:12:01 +0800

    图片oss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+import Home from './container/Home'
 // normalize.css 不带反斜杠，优先从src取，否则从node_modules
 import 'normalize.css';
-import './style.scss';
+// import './style.scss';
 // import styles from './style.module.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        low code
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/container/Home/components/Banner/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './style.module.scss'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className={style.banner}>
+                Home Banner
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
